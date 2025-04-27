c66c99dff0ddae62ec7aea9933188c3485768450
Noah Ojile <noahojile04@gmail.com>
2025-04-27 23:51:23 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Interactive Piano',
+  description: 'a simple app to play the piano',
+  generator: 'Noah Ojile',
 }
 
 export default function RootLayout({
